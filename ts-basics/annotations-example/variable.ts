a080cae9694823af74220974bee6b13ff0695499
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-30 15:19:19 +0530

    annotations usage example in specific conditions

--- ts-basics/annotations-example/variable.ts
+++ ts-basics/annotations-example/variable.ts
@@ -32,3 +32,30 @@ let point: { x: number; y: number } = { x: 1, y: 2 };
 const logNumber: (i: number) => void = (i: number) => {
 	console.log(i);
 };
+
+//when to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x":10,"y":20}';
+const coords: { x: number; y: number } = JSON.parse(json);
+console.log(coords); //{ x: 10, y: 20 }
+
+// 2) When we declare a variable on one line and initialize it later
+let colours = ["red", "green", "blue"];
+let found: boolean;
+
+for (let i = 0; i < colours.length; i++) {
+	if (colours[i] === "green") {
+		found = true;
+	}
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let nums = [-10, -1, 12];
+// let numAboveZero = false; //if number is above 0 then assign it to this variable and if no number is above 0 then assign false to this variable
+let numAboveZero: boolean | number = false;
+
+for (let i = 0; i < nums.length; i++) {
+	if (nums[i] > 0) {
+		numAboveZero = nums[i];
+	}
+}
